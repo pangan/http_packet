d21100661d352ecf4ab1abc680b256e44ebbffe1
Amir Mofakhar <amofakhar@op5.com>
2017-12-15 11:53:31 +0100

    update more

--- src/server/server.py
+++ src/server/server.py
@@ -7,6 +7,7 @@ from wsgiref import simple_server
 
 log = get_logger('test.log')
 
+
 class Resources(object):
     def on_post(self, req, resp):
 
@@ -14,19 +15,18 @@ class Resources(object):
             received_data = req.stream.read()
             json_received = json.loads(received_data)
             received_size = len(received_data)
-            received_json_size = len(json_received['a'])
-            doc = {'received_bytes': received_size,
-                   'received_json_value_size': received_json_size}
+            #received_json_size = len(json_received['a'])
+            doc = {'received_bytes': received_size}
             #log.debug('received data: {0}'.format(received_data))
             log.debug('received_data_size: {0}'.format(received_size))
-            log.debug('received_json_value_size: {0}'.format(received_json_size))
+            #log.debug('received_json_value_size: {0}'.format(received_json_size))
 
             resp.status = falcon.HTTP_200
             resp.body = json.dumps(doc, ensure_ascii=False)
         except Exception, e:
             log.error(e)
 
-def myapp():
+def myapp(a=None):
     app = falcon.API()
     resource = Resources()
     app.add_route('/', resource)
@@ -39,5 +39,5 @@ if __name__ == '__main__':
     log.info('server started on port {0}!'.format(server_port))
     wsgi_app = myapp()
 
-    httpd = simple_server.make_server('0.0.0.0', server_port, wsgi_app)
+    httpd = simple_server.make_server('127.0.0.1', server_port, wsgi_app)
     httpd.serve_forever()
